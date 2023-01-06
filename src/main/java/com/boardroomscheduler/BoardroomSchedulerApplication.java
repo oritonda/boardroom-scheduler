9e5def7c288f83aedddfc9fcb2174e05108d1fa2
Oritonda <oritonda.ramug@gmail.com>
2023-01-06 21:42:26 +0200

    starting point

---
+++ src/main/java/com/boardroomscheduler/BoardroomSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.boardroomscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardroomSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardroomSchedulerApplication.class, args);
+	}
+
+}
